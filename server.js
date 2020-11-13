368cbc3897acff86bc3c78d5004438738f16c703
ironsocks <brach.thomasja@gmail.com>
2020-11-13 11:10:24 +0100

    initial react setup

--- server.js
+++ server.js
@@ -18,5 +18,5 @@ app.use("/api/users", require("./routes/users"));
 app.use("/api/contacts", require("./routes/contacts"));
 app.use("/api/auth", require("./routes/auth"));
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}.`));
