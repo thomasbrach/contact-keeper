fffff3a1de54c2e75057e6243e968f8fe4f1df0a
ironsocks <brach.thomasja@gmail.com>
2020-11-13 10:54:16 +0100

    backend done

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require("express");
+const connectDB = require("./config/db");
+
 const app = express();
 
-app.get("/", (req, res) =>
-  res.json({ msg: "Welcome to the ContactKeeper API!" })
-);
+// Connect DB
+connectDB();
+
+// Init Middleware
+app.use(express.json({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.json({ msg: "Welcome to the ContactKeeper API!" });
+});
 
 // Define Routes
 app.use("/api/users", require("./routes/users"));
